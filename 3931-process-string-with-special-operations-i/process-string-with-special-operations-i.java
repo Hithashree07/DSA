155dd0516ba4bd0ac5025c6049d9f3cf27631a65
Hithashree <hithamadival@gmail.com>
2026-06-16 16:53:27 +0530

    Time: 4 ms (76.47%) | Memory: 55.2 MB (64.71%) - LeetSync

---
+++ 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String processStr(String s) {
+        StringBuilder res=new StringBuilder();
+        int n=s.length();
+        for(int i=0;i<n;i++){
+            char ch=s.charAt(i);
+            if(ch=='*'){
+                if(res.length()!=0){
+                    res.deleteCharAt(res.length()-1);
+                }
+            }
+            else if(ch=='#'){
+                res.append(res);
+            }
+            else if(ch=='%'){
+                res.reverse();
+            }
+            else if(ch>='a'&&ch<='z'){
+                res.append(ch);
+            }
+        }
+        return res.toString();
+    }
+}
