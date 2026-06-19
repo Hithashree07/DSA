ac7f9e4d93030cfc82a99196fb82eb128a360d8f
Hithashree <hithamadival@gmail.com>
2026-06-19 16:50:59 +0530

    Time: 6 ms (0.91%) | Memory: 43.5 MB (5.08%) - LeetSync

--- 231-power-of-two/power-of-two.java
+++ 231-power-of-two/power-of-two.java
@@ -1,5 +1,19 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-     return ((n>0)&&(n&(n-1))==0)?true:false;
+     String binary="";
+     while(n>0){
+        binary=n%2+binary;
+        n=n/2;
+     }
+     int count=0;
+     for(char ch:binary.toCharArray()){
+        if(ch=='1'){
+            count++;
+        }
+     }
+     if(count==1){
+        return true;
+     }
+     return false;
     }
 }
